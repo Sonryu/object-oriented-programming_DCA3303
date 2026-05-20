b158b032382824510eb10e1173cd2e0b2a1d49d2
Ramon Watson Vilar <watsonvilar@gmail.com>
2026-05-20 15:39:24 -0300

    aula 01

---
+++ aula01/vetor2d.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Vetor2d{
+	private:
+	       	float x, y;
+	
+	public:
+	       void setX(float x_){
+	       x = x_;
+	       }	
+	       
+	       float getX(){
+	       return x;
+	       }
+
+};
+
+int main(void){
+Vetor2d v;
+
+v.setX(3);
+
+std::cout << v.getX() << "\n";
+
+}
